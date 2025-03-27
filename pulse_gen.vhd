eda3946795eaa64f1960f1c0070853c200e5009e
mecargill <cargill.matthew@gmail.com>
2025-03-27 17:29:43 -0400

    Made a simple brightness thing
    Worked in sim, so will merge to dev

--- pulse_gen.vhd
+++ pulse_gen.vhd
@@ -19,8 +19,22 @@ end pulse_gen;
 
 
 architecture a of pulse_gen is
-
+	signal count : std_logic_vector(5 downto 0);
 begin
-	leds <= "0000000000";
+	process(clk12MHz)
+	begin
+		if rising_edge(clk12MHz) then
+			for i in 0 to 9 loop
+				if count = "111111" then
+					--64 clock edges = 65 brightness options, we put the jump at the end so full brighness is possible
+				elsif count = brightnesses(i) then
+					leds(i) <= '0';
+				elsif count = "000000" then
+					leds(i) <= '1';
+				end if;
+			end loop;
+			count <= count + 1;		
+		end if;
+	end process;
 
 end a;
